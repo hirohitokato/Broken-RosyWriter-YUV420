d59718b79c020e43e1ae236645bc0d7f2ffccb5d
Hirohito Kato <support@cielo.rojo.jp>
2012-04-10 10:29:13 +0900

    First commit

---
+++ Classes/PreviewView.m
@@ -0,0 +1,366 @@
+/*
+     File: PreviewView.m
+ Abstract: The OpenGL ES view, responsible for creating a CVOpenGLESTexture from each CVImageBuffer and displaying the texture on the screen.
+  Version: 1.2
+ 
+ Disclaimer: IMPORTANT:  This Apple software is supplied to you by Apple
+ Inc. ("Apple") in consideration of your agreement to the following
+ terms, and your use, installation, modification or redistribution of
+ this Apple software constitutes acceptance of these terms.  If you do
+ not agree with these terms, please do not use, install, modify or
+ redistribute this Apple software.
+ 
+ In consideration of your agreement to abide by the following terms, and
+ subject to these terms, Apple grants you a personal, non-exclusive
+ license, under Apple's copyrights in this original Apple software (the
+ "Apple Software"), to use, reproduce, modify and redistribute the Apple
+ Software, with or without modifications, in source and/or binary forms;
+ provided that if you redistribute the Apple Software in its entirety and
+ without modifications, you must retain this notice and the following
+ text and disclaimers in all such redistributions of the Apple Software.
+ Neither the name, trademarks, service marks or logos of Apple Inc. may
+ be used to endorse or promote products derived from the Apple Software
+ without specific prior written permission from Apple.  Except as
+ expressly stated in this notice, no other rights or licenses, express or
+ implied, are granted by Apple herein, including but not limited to any
+ patent rights that may be infringed by your derivative works or by other
+ works in which the Apple Software may be incorporated.
+ 
+ The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
+ MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+ THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+ FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
+ OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+ 
+ IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+ OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+ MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
+ AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+ STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
+ POSSIBILITY OF SUCH DAMAGE.
+ 
+ Copyright (C) 2011 Apple Inc. All Rights Reserved.
+ 
+ */
+/**
+ * AppleのサンプルRosyWriterとGLCameraRippleをベースに、得られるサンプルバッファのデータ形式を
+ * BGRA32からYUV420に変更。ここではRosyWriter描画処理部分(BGRA32)にGLCameraRipple(YUV420)の処理を移植
+ */
+
+#import <QuartzCore/CAEAGLLayer.h>
+#import "PreviewView.h"
+#include "ShaderUtilities.h"
+
+// Uniform index.
+enum
+{
+    UNIFORM_Y,
+    UNIFORM_UV,
+    NUM_UNIFORMS
+};
+GLint uniforms[NUM_UNIFORMS];
+
+enum {
+    ATTRIB_VERTEX,
+    ATTRIB_TEXTUREPOSITON,
+    NUM_ATTRIBUTES
+};
+
+@implementation PreviewView
+
++ (Class)layerClass 
+{
+    return [CAEAGLLayer class];
+}
+
+- (const GLchar *)readFile:(NSString *)name
+{
+    NSString *path;
+    const GLchar *source;
+    
+    path = [[NSBundle mainBundle] pathForResource:name ofType: nil];
+    source = (GLchar *)[[NSString stringWithContentsOfFile:path
+												  encoding:NSUTF8StringEncoding error:nil]
+						UTF8String];
+    
+    return source;
+}
+
+- (BOOL)initializeBuffers
+{
+	BOOL success = YES;
+	
+	glDisable(GL_DEPTH_TEST);
+    
+    glGenFramebuffers(1, &frameBufferHandle);
+    glBindFramebuffer(GL_FRAMEBUFFER, frameBufferHandle);
+    
+    glGenRenderbuffers(1, &colorBufferHandle);
+    glBindRenderbuffer(GL_RENDERBUFFER, colorBufferHandle);
+    
+    [oglContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:(CAEAGLLayer *)self.layer];
+    
+	glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &renderBufferWidth);
+    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &renderBufferHeight);
+    
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, colorBufferHandle);
+	if(glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        NSLog(@"Failure with framebuffer generation");
+		success = NO;
+	}
+    
+    //  Create a new CVOpenGLESTexture cache
+    CVReturn err = CVOpenGLESTextureCacheCreate(kCFAllocatorDefault, NULL, oglContext, NULL, &videoTextureCache);
+    if (err) {
+        NSLog(@"Error at CVOpenGLESTextureCacheCreate %d", err);
+        success = NO;
+    }
+    
+    // Load vertex and fragment shaders
+    const GLchar *vertSrc = [self readFile:@"Shader.vsh"];
+    const GLchar *fragSrc = [self readFile:@"Shader.fsh"];
+    
+    // attributes
+    GLint attribLocation[NUM_ATTRIBUTES] = {
+        ATTRIB_VERTEX, ATTRIB_TEXTUREPOSITON,
+    };
+    GLchar *attribName[NUM_ATTRIBUTES] = {
+        "position", "textureCoordinate",			
+    };
+	GLchar *uniformName[NUM_UNIFORMS] = {
+		"SamplerY", "SamplerUV",
+	};
+
+    glueCreateProgram(vertSrc, fragSrc,
+                      NUM_ATTRIBUTES, (const GLchar **)&attribName[0], attribLocation,
+                      NUM_UNIFORMS, (const GLchar **)&uniformName[0], uniforms,
+                      &program);
+
+    if (!program)
+        success = NO;
+    
+    return success;
+}
+
+-(id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self != nil) {
+		// Use 2x scale factor on Retina displays.
+		self.contentScaleFactor = [[UIScreen mainScreen] scale];
+
+        // Initialize OpenGL ES 2
+        CAEAGLLayer* eaglLayer = (CAEAGLLayer *)self.layer;
+        eaglLayer.opaque = YES;
+        eaglLayer.drawableProperties = [NSDictionary dictionaryWithObjectsAndKeys:
+                                        [NSNumber numberWithBool:NO], kEAGLDrawablePropertyRetainedBacking,
+                                        kEAGLColorFormatRGBA8, kEAGLDrawablePropertyColorFormat,
+                                        nil];
+        oglContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2];
+        if (!oglContext || ![EAGLContext setCurrentContext:oglContext]) {
+            NSLog(@"Problem with OpenGL context.");
+            [self release];
+            
+            return nil;
+        }
+    }
+	
+    return self;
+}
+
+- (void)renderWithSquareVertices:(const GLfloat*)squareVertices textureVertices:(const GLfloat*)textureVertices
+{
+    // Use shader program.
+    glUseProgram(program);
+    
+    glUniform1i(uniforms[UNIFORM_Y], 0);
+    glUniform1i(uniforms[UNIFORM_UV], 1);
+
+    // Update attribute values.
+	glVertexAttribPointer(ATTRIB_VERTEX, 2, GL_FLOAT, 0, 0, squareVertices);
+	glEnableVertexAttribArray(ATTRIB_VERTEX);
+	glVertexAttribPointer(ATTRIB_TEXTUREPOSITON, 2, GL_FLOAT, 0, 0, textureVertices);
+	glEnableVertexAttribArray(ATTRIB_TEXTUREPOSITON);
+    
+    // Update uniform values if there are any
+    
+    // Validate program before drawing. This is a good check, but only really necessary in a debug build.
+    // DEBUG macro must be defined in your debug configurations if that's not already the case.
+#if defined(DEBUG)    
+    if (glueValidateProgram(program) != 0) {
+        NSLog(@"Failed to validate program: %d", program);
+        return;
+    }    
+#endif
+	
+	glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    
+    // Present
+    glBindRenderbuffer(GL_RENDERBUFFER, colorBufferHandle);
+    [oglContext presentRenderbuffer:GL_RENDERBUFFER];
+}
+
+- (CGRect)textureSamplingRectForCroppingTextureWithAspectRatio:(CGSize)textureAspectRatio toAspectRatio:(CGSize)croppingAspectRatio
+{
+	CGRect normalizedSamplingRect = CGRectZero;	
+	CGSize cropScaleAmount = CGSizeMake(croppingAspectRatio.width / textureAspectRatio.width, croppingAspectRatio.height / textureAspectRatio.height);
+	CGFloat maxScale = fmax(cropScaleAmount.width, cropScaleAmount.height);
+	CGSize scaledTextureSize = CGSizeMake(textureAspectRatio.width * maxScale, textureAspectRatio.height * maxScale);
+	
+	if ( cropScaleAmount.height > cropScaleAmount.width ) {
+		normalizedSamplingRect.size.width = croppingAspectRatio.width / scaledTextureSize.width;
+		normalizedSamplingRect.size.height = 1.0;
+	}
+	else {
+		normalizedSamplingRect.size.height = croppingAspectRatio.height / scaledTextureSize.height;
+		normalizedSamplingRect.size.width = 1.0;
+	}
+	// Center crop
+	normalizedSamplingRect.origin.x = (1.0 - normalizedSamplingRect.size.width)/2.0;
+	normalizedSamplingRect.origin.y = (1.0 - normalizedSamplingRect.size.height)/2.0;
+	
+	return normalizedSamplingRect;
+}
+
+- (void)cleanUpTextures
+{    
+    if (lumaTexture)
+    {
+        CFRelease(lumaTexture);
+        lumaTexture = NULL;        
+    }
+    
+    if (chromaTexture)
+    {
+        CFRelease(chromaTexture);
+        chromaTexture = NULL;
+    }
+    
+    // Periodic texture cache flush every frame
+    CVOpenGLESTextureCacheFlush(videoTextureCache, 0);
+}
+
+- (void)displayPixelBuffer:(CVImageBufferRef)pixelBuffer 
+{    
+	if (frameBufferHandle == 0) {
+		BOOL success = [self initializeBuffers];
+		if ( !success ) {
+			NSLog(@"Problem initializing OpenGL buffers.");	
+		}
+	}
+
+    if (videoTextureCache == NULL)
+        return;
+	
+    [self cleanUpTextures];
+
+	size_t frameWidth = CVPixelBufferGetWidth(pixelBuffer);
+	size_t frameHeight = CVPixelBufferGetHeight(pixelBuffer);
+
+	CVReturn err;
+    // Y-plane
+    glActiveTexture(GL_TEXTURE0);
+    err = CVOpenGLESTextureCacheCreateTextureFromImage(kCFAllocatorDefault, 
+                                                       videoTextureCache,
+                                                       pixelBuffer,
+                                                       NULL,
+                                                       GL_TEXTURE_2D,
+                                                       GL_RED_EXT,
+                                                       frameWidth,
+                                                       frameHeight,
+                                                       GL_RED_EXT,
+                                                       GL_UNSIGNED_BYTE,
+                                                       0,
+                                                       &lumaTexture);
+    if (err) 
+    {
+        NSLog(@"Error at CVOpenGLESTextureCacheCreateTextureFromImage %d", err);
+    }   
+    
+    glBindTexture(CVOpenGLESTextureGetTarget(lumaTexture), CVOpenGLESTextureGetName(lumaTexture));
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); 
+    
+    // UV-plane
+    glActiveTexture(GL_TEXTURE1);
+    err = CVOpenGLESTextureCacheCreateTextureFromImage(kCFAllocatorDefault, 
+                                                       videoTextureCache,
+                                                       pixelBuffer,
+                                                       NULL,
+                                                       GL_TEXTURE_2D,
+                                                       GL_RG_EXT,
+                                                       frameWidth/2,
+                                                       frameHeight/2,
+                                                       GL_RG_EXT,
+                                                       GL_UNSIGNED_BYTE,
+                                                       1,
+                                                       &chromaTexture);
+    if (err) 
+    {
+        NSLog(@"Error at CVOpenGLESTextureCacheCreateTextureFromImage %d", err);
+    }
+    
+    glBindTexture(CVOpenGLESTextureGetTarget(chromaTexture), CVOpenGLESTextureGetName(chromaTexture));
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); 
+
+    glBindFramebuffer(GL_FRAMEBUFFER, frameBufferHandle);
+    
+    // Set the view port to the entire view
+    glViewport(0, 0, renderBufferWidth, renderBufferHeight);
+	
+    static const GLfloat squareVertices[] = {
+        -1.0f, -1.0f,
+        1.0f, -1.0f,
+        -1.0f,  1.0f,
+        1.0f,  1.0f,
+    };
+
+	// The texture vertices are set up such that we flip the texture vertically.
+	// This is so that our top left origin buffers match OpenGL's bottom left texture coordinate system.
+	CGRect textureSamplingRect = [self textureSamplingRectForCroppingTextureWithAspectRatio:CGSizeMake(frameWidth, frameHeight) toAspectRatio:self.bounds.size];
+	GLfloat textureVertices[] = {
+		CGRectGetMinX(textureSamplingRect), CGRectGetMaxY(textureSamplingRect),
+		CGRectGetMaxX(textureSamplingRect), CGRectGetMaxY(textureSamplingRect),
+		CGRectGetMinX(textureSamplingRect), CGRectGetMinY(textureSamplingRect),
+		CGRectGetMaxX(textureSamplingRect), CGRectGetMinY(textureSamplingRect),
+	};
+	
+    // Draw the texture on the screen with OpenGL ES 2
+    [self renderWithSquareVertices:squareVertices textureVertices:textureVertices];
+}
+
+- (void)dealloc 
+{
+	if (frameBufferHandle) {
+        glDeleteFramebuffers(1, &frameBufferHandle);
+        frameBufferHandle = 0;
+    }
+	
+    if (colorBufferHandle) {
+        glDeleteRenderbuffers(1, &colorBufferHandle);
+        colorBufferHandle = 0;
+    }
+	
+    if (program) {
+        glDeleteProgram(program);
+        program = 0;
+    }
+
+	[self cleanUpTextures];
+
+    if (videoTextureCache) {
+        CFRelease(videoTextureCache);
+        videoTextureCache = 0;
+    }
+    
+    [super dealloc];
+}
+
+@end
